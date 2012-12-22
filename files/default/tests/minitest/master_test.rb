44ce854c2aab17d42c45b38940ed08166d08535b
Sean Porter <portertech@gmail.com>
2012-12-21 22:49:55 -0800

    removed test-kitchen

--- files/default/tests/minitest/master_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../support/helpers', __FILE__)
-
-describe "monitor::master" do
-  include Helpers::Monitor
-
-  it "runs the sensu server" do
-    service("sensu-server").must_be_running
-  end
-end
--- files/default/tests/minitest/support/helpers.rb
+++
@@ -1,9 +0,0 @@
-module Helpers
-  module Monitor
-    require 'chef/mixin/shell_out'
-    include Chef::Mixin::ShellOut
-    include MiniTest::Chef::Assertions
-    include MiniTest::Chef::Context
-    include MiniTest::Chef::Resources
-  end
-end
