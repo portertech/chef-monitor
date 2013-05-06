cfdff1b7008e950e8137928c30428c8bfcda9d19
Sean Porter <portertech@gmail.com>
2013-05-06 12:16:40 -0700

    prep for the first release

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,14 @@ description      "A cookbook for monitoring services, using Sensu, a monitoring 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-supports "ubuntu"
+%w[
+  ubuntu
+  debian
+  centos
+  redhat
+  fedora
+].each do |os|
+  supports os
+end
 
 depends "sensu"
