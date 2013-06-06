a0e7ad4d45102251022b36708bf68473a1f577bc
Sean Porter <portertech@gmail.com>
2013-06-05 23:58:56 -0700

    monitor haproxy, add to stack suites (haproxy)

---
+++ files/default/plugins/check-haproxy.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/env ruby
+#
+# HAProxy Check
+# ===
+#
+# Defaults to checking if ALL services in the given group are up; with
+# -1, checks if ANY service is up. with -A, checks all groups.
+#
+# Updated: To add -S to allow for different named sockets 
+#
+# Copyright 2011 Sonian, Inc.
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
+
+require "rubygems" if RUBY_VERSION < "1.9.0"
+require "sensu-plugin/check/cli"
+
+def silent_require(buggy_gem)
+  dup_stderr = STDERR.dup
+  STDERR.reopen("/dev/null")
+  unless require(buggy_gem)
+    echo "haproxy gem not installed"
+    exit 1
+  end
+  STDERR.reopen(dup_stderr)
+end
+
+silent_require "haproxy"
+
+class CheckHAProxy < Sensu::Plugin::Check::CLI
+
+  option :warn_percent,
+    :short => "-w PERCENT",
+    :boolean => true,
+    :default => 50,
+    :proc => proc {|a| a.to_i },
+    :description => "Warning Percent, default: 50"
+
+  option :crit_percent,
+    :short => "-c PERCENT",
+    :boolean => true,
+    :default => 25,
+    :proc => proc {|a| a.to_i },
+    :description => "Critical Percent, default: 25"
+
+  option :all_services,
+    :short => "-A",
+    :boolean => true,
+    :description => "Check ALL Services, flag enables"
+
+  option :missing_ok,
+    :short => "-m",
+    :boolean => true,
+    :description => "Missing OK, flag enables"
+
+  option :service,
+    :short => "-s SVC",
+    :description => "Service Name to Check"
+
+  option :socket,
+    :short => "-S SOCKET",
+    :default => "/var/run/haproxy.sock",
+    :description => "Path to HAProxy Socket, default: /var/run/haproxy.sock"
+
+  def run
+    if config[:service] or config[:all_services]
+      services = get_services
+    else
+      unknown "No service specified"
+    end
+
+    if services.empty?
+      message "No services matching /#{config[:service]}/"
+      if config[:missing_ok]
+        ok
+      else
+        warning
+      end
+    else
+      percent_up = 100 * services.select {|svc| svc[:status] == "UP" }.size / services.size
+      failed_names = services.reject {|svc| svc[:status] == "UP" }.map {|svc| svc[:svname] }
+      services_down = failed_names.empty? ? "" : ", DOWN: #{failed_names.join(', ')}"
+      message "UP: #{percent_up}% of #{services.size} /#{config[:service]}/ services" + services_down
+      if percent_up < config[:crit_percent]
+        critical
+      elsif percent_up < config[:warn_percent]
+        warning
+      else
+        ok
+      end
+    end
+  end
+
+  def get_services
+    haproxy = HAProxy.read_stats(config[:socket])
+    if config[:all_services]
+      haproxy.stats
+    else
+      haproxy.stats.select do |svc|
+        svc[:pxname] =~ /#{config[:service]}/
+      end.reject do |svc|
+        ["FRONTEND", "BACKEND"].include?(svc[:svname])
+      end
+    end
+  end
+
+end
---
+++ recipes/haproxy.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: monitor
+# Recipe:: haproxy
+#
+# Copyright 2013, Sean Porter Consulting
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "monitor::_haproxy"
+
+sensu_check "check_haproxy" do
+  command "check-haproxy.rb -s :::haproxy_services::: -w :::haproxy_warning|75::: -c :::haproxy_critical|50:::"
+  handlers ["default"]
+  standalone true
+  interval 30
+end
