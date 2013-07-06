bbb7209aeb9d8c4d3b61da346e4c1c135701278d
Sean Porter <portertech@gmail.com>
2013-07-06 13:26:56 -0700

    added the chef_node handler, an example of chef integration

---
+++ recipes/_chef_node_handler.rb
@@ -0,0 +1,57 @@
+#
+# Cookbook Name:: monitor
+# Recipe:: _chef_node_handler
+#
+# Copyright 2013, Sean Porter Consulting
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+node.override['monitor']['sudo_commands'] =
+  node['monitor']['sudo_commands'].push("/etc/sensu/handlers/chef_node.rb")
+
+include_recipe "monitor::_sudo"
+
+sensu_gem "spice" do
+  version "1.0.6"
+end
+
+sensu_gem "rest-client"
+
+cookbook_file "/etc/sensu/handlers/chef_node.rb" do
+  source "handlers/chef_node.rb"
+  mode 0755
+end
+
+sensu_snippet "chef" do
+  content(
+    :server_url => Chef::Config[:chef_server_url],
+    :client_name => Chef::Config[:node_name],
+    :client_key => Chef::Config[:client_key],
+    :verify_ssl => false
+  )
+end
+
+sensu_filter "keepalives" do
+  attributes(
+    :check => {
+      :name => "keepalive"
+    }
+  )
+end
+
+sensu_handler "chef_node" do
+  type "pipe"
+  command "sudo chef_node.rb"
+  filters ["keepalives"]
+end
