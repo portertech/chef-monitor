ed7b98aa084c740efa022460c1a1be68c09e1c5e
Seth Lindberg <seth@womply.com>
2014-08-18 16:56:19 -0700

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "portertech@gmail.com"
 license          "Apache 2.0"
 description      "A cookbook for monitoring services, using Sensu, a monitoring framework."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 
 %w[
   ubuntu
